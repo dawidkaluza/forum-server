6faf8011e093e5e1a93b571b46ac953feed6368f
dawidkaluza <daw.kaluza@gmail.com>
2021-01-29 22:20:53 +0100

    Remove id field

--- src/main/java/pl/dkaluza/forum/core/exceptions/entity/EntityNotFoundException.java
+++ src/main/java/pl/dkaluza/forum/core/exceptions/entity/EntityNotFoundException.java
@@ -4,24 +4,16 @@ import java.io.Serializable;
 
 public class EntityNotFoundException extends RuntimeException {
     private static final long serialVersionUID = -3025566621391895011L;
-    private final Serializable id;
 
     public EntityNotFoundException(Class<?> entityClass, Serializable id) {
         super("Cant find entity " + entityClass.getCanonicalName() + " with id " + id);
-        this.id = id;
     }
 
-    public EntityNotFoundException(String reason, Serializable id) {
+    public EntityNotFoundException(String reason) {
         super(reason);
-        this.id = id;
     }
 
-    public EntityNotFoundException(String reason, Serializable id, Throwable throwable) {
+    public EntityNotFoundException(String reason, Throwable throwable) {
         super(reason, throwable);
-        this.id = id;
-    }
-
-    public Serializable getId() {
-        return id;
     }
 }
