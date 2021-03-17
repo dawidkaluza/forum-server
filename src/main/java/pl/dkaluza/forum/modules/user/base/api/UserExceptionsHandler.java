df4bcca3bbddfc389318655194c2734891ccd2e2
dawidkaluza <daw.kaluza@gmail.com>
2021-03-17 06:53:12 +0100

    Improve exceptions and handling them

--- src/main/java/pl/dkaluza/forum/modules/user/base/api/UserExceptionsHandler.java
+++ src/main/java/pl/dkaluza/forum/modules/user/base/api/UserExceptionsHandler.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import pl.dkaluza.forum.core.api.exceptionsHandler.ExceptionsHandler;
 import pl.dkaluza.forum.core.api.response.LocaleFieldErrorMapper;
+import pl.dkaluza.forum.core.api.response.ResponseError;
 import pl.dkaluza.forum.core.api.response.ResponseFieldsError;
 import pl.dkaluza.forum.modules.user.base.exceptions.EmailAlreadyExistsException;
-import pl.dkaluza.forum.modules.user.base.exceptions.InvalidPasswordException;
 import pl.dkaluza.forum.modules.user.base.exceptions.NameAlreadyExistsException;
 import pl.dkaluza.forum.modules.user.base.exceptions.UserNotFoundException;
 
@@ -23,19 +23,18 @@ public class UserExceptionsHandler implements ExceptionsHandler {
         this.localeFieldErrorMapper = localeFieldErrorMapper;
     }
 
-    @ExceptionHandler(InvalidPasswordException.class)
-    public ResponseEntity<?> invalidPasswordExceptionHandler(InvalidPasswordException e) {
-        return new ResponseEntity<>("Password must have between 3 and 32 signs", HttpStatus.BAD_REQUEST);
-    }
-
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<?> userNotFoundExceptionHandler(UserNotFoundException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<?> userNotFoundExceptionHandler() {
+        return new ResponseError(HttpStatus.NOT_FOUND, "User not found")
+            .toResponseEntity();
     }
 
     @ExceptionHandler(NameAlreadyExistsException.class)
-    public ResponseEntity<?> nameAlreadyExistsExceptionHandler(NameAlreadyExistsException e) {
-        return new ResponseEntity<>("User with name=" + e.getName() + " already exists", HttpStatus.CONFLICT);
+    public ResponseEntity<?> nameAlreadyExistsExceptionHandler(WebRequest request) {
+        return new ResponseFieldsError(HttpStatus.CONFLICT, "Invalid params")
+            .add(localeFieldErrorMapper.map(
+                "name", "user.register.nameAlreadyExists", request.getLocale()
+            )).toResponseEntity();
     }
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
--- src/main/java/pl/dkaluza/forum/modules/user/base/exceptions/InvalidPasswordException.java
+++
@@ -1,9 +0,0 @@
-package pl.dkaluza.forum.modules.user.base.exceptions;
-
-public class InvalidPasswordException extends RuntimeException {
-    private static final long serialVersionUID = -1542907228548756522L;
-
-    public InvalidPasswordException() {
-        super("Password must have between 3 and 32 signs");
-    }
-}
