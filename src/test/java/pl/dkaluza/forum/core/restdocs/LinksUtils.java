183803235093791ed6b50250def170f389b69400
dawidkaluza <daw.kaluza@gmail.com>
2021-03-23 17:48:16 +0100

    Add useful restdocs utils

--- src/test/java/pl/dkaluza/forum/core/restdocs/LinksUtils.java
+++ src/test/java/pl/dkaluza/forum/core/restdocs/LinksUtils.java
@@ -5,11 +5,20 @@ import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.restdocs.snippet.Attributes;
 
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath;
 
 public class LinksUtils {
+    public static FieldDescriptor embeddedFieldDescriptor() {
+        return fieldWithPath("_embedded").description("Child resources, where keys are relations to them");
+    }
+
+    public static FieldDescriptor linksFieldDescriptor(String path) {
+        return subsectionWithPath(path).description("Links to resources");
+    }
+
     public static FieldDescriptor linksFieldDescriptor() {
-        return subsectionWithPath("_links").description("Links to resources");
+        return linksFieldDescriptor("_links");
     }
 
     public static LinkDescriptor selfLinkDescriptor() {
@@ -20,20 +29,24 @@ public class LinksUtils {
         return linkWithRel("curies").description("Curies that describe custom HAL relations");
     }
 
-    public static LinkDescriptor firstLinkDescriptor() {
-        return linkWithRel("first").description("First element of the whole collection");
+    public static LinkDescriptor firstPageLinkDescriptor() {
+        return linkWithRel("first").optional().description("Link to first page");
     }
 
-    public static LinkDescriptor lastLinkDescriptor() {
-        return linkWithRel("last").description("Last element of the whole collection");
+    public static LinkDescriptor prevPageLinkDescriptor() {
+        return linkWithRel("prev").optional().description("Link to previous page");
     }
 
-    public static LinkDescriptor prevLinkDescriptor() {
-        return linkWithRel("prev").description("Previous page of the collection");
+    public static LinkDescriptor selfPageLinkDescriptor() {
+        return linkWithRel("self").description("Link to current page");
     }
 
-    public static LinkDescriptor nextLinkDescriptor() {
-        return linkWithRel("next").description("Next page of the collection");
+    public static LinkDescriptor nextPageLinkDescriptor() {
+        return linkWithRel("next").optional().description("Link to next page");
+    }
+
+    public static LinkDescriptor lastPageLinkDescriptor() {
+        return linkWithRel("last").optional().description("Link to last page");
     }
 
     public static Attributes.Attribute docsAttribute(String uri) {
